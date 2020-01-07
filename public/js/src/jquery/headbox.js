3814ac894e4d0909a36b2b8161e306eb5a7b5fc9
jexists <jexists486@gmail.com>
2020-01-07 11:09:59 +0900

    gnb line on 200107

--- public/js/src/jquery/headbox.js
+++ public/js/src/jquery/headbox.js
@@ -1,15 +1,35 @@
 (function($){
-
+ const wrap = $('#wrap');
  const gnb = $('.gnb');
- const gnbLiLink = gnb.children('ul').children('li').children('a');
+ const gnbLi = gnb.children('ul').children('li');
+ const gnbLiLink = gnbLi.children('a');
  const gnbConts = gnb.find('.gnb_conts');
+ const gnbLine = gnb.find('.gnbline');
+ const gnbLiOff = [];
+ const gnbOff = gnb.offset().left;
+
+ // console.log(gnbConts.length);
+ console.log(gnbOff);
+ for(let i=0; i<gnbLi.length; i++){
+ 	gnbLiOff[i] = gnbLi.eq(i).offset().left;	
+ }
+ 	console.log(gnbLiOff);
+	console.log(gnbOff);
+
 
  gnbLiLink.on('mouseenter focus',function(){
  	gnbConts.stop().slideUp();
  	$(this).next('.gnb_conts').stop().slideDown();
+ 	gnbLine.stop().fadeIn();
+
+ 	let i = $(this).parent('li').index();
+ 	// console.log(i);
+	let myOf = (gnbLiOff[i] - gnbOff) +'px';
+	gnbLine.stop().animate({left:myOf});
  });
 
- gnbConts.on('mouseleave',function(){
+ gnb.on('mouseleave',function(){
  	gnbConts.stop().slideUp();
+ 	gnbLine.stop().fadeOut();
  });
 })(jQuery);
