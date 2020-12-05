a7752d913e62a4188193a83e2e6925ed650b2ec9
jexists <jexists486@gmail.com>
2020-12-05 14:26:27 +0900

    reservationBox fixed

--- public/js/src/jquery/siteindi.js
+++ public/js/src/jquery/siteindi.js
@@ -1,6 +1,22 @@
 
 
 (function($){
+	const siteIndicator = $('#siteIndicator');
+	const indiLi = siteIndicator.find('li');
+	const indiLiA = indiLi.find('a');
+	const wrap = $('#wrap');
+	const reserveBox = $('#reserveBox');
+	const inquiryBox = $('#inquiryBox');
+	const checkinBox = $('#checkinBox');
+	const timeBox = $('#timeBox');
+	const eventBox = $('#eventBox');
+
+	indiLiA.on('click',function(e){
+		// e.preventDefault();
+		indiLi.removeClass('on');
+		$(this).parent('li').addClass('on');
+	});
+
 
 
 })(jQuery);
